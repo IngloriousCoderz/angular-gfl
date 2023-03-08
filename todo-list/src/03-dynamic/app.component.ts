40b0a3281c278651f76bd6171978c46ba91c4808
IceOnFire <antony.mistretta@gmail.com>
2023-03-08 18:02:33 +0100

    Create Angular apps up to Reactive Forms

--- todo-list/src/03-dynamic/app.component.ts
+++ todo-list/src/03-dynamic/app.component.ts
@@ -1,11 +1,4 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-
-interface Task {
-  id: number;
-  text?: string | null;
-  completed?: boolean | null;
-}
 
 @Component({
   selector: 'app-root',
@@ -13,26 +6,20 @@ interface Task {
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  form;
+  text = '';
 
-  tasks: Task[] = [
+  tasks = [
     { id: 1, text: 'Learn Angular', completed: true },
     { id: 2, text: 'Look for a job', completed: false },
     { id: 3, text: 'Forget everything' },
   ];
 
-  constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({
-      text: [''],
-    });
-  }
-
   handleSubmit(event: SubmitEvent) {
     event.preventDefault();
 
     const maxId = this.tasks.length ? this.tasks[this.tasks.length - 1].id : 0;
-    this.tasks.push({ id: maxId + 1, text: this.form.value.text });
-    this.form.patchValue({ text: '' });
+    this.tasks.push({ id: maxId + 1, text: this.text });
+    this.text = '';
   }
 
   handleSpanClick(index: number) {
